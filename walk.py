62e8852059b6b1320e1b6a547f71d84c7533c182
Ben <ben@debainlarge.ben>
2015-03-21 12:41:54 -0400

    func defs

--- walk.py
+++ walk.py
@@ -1,13 +1,45 @@
 import os
 
-def walk(d, i=0):
-	for c in os.listdir(d):
-		print(i*"  " + c)
-		walk(d + '/' + c, i + 1)
+class Definition:
+	def __init__(self, dirname):
+		self.dirname = dirname
+		parts = dirname.split('_')
+		self.name = parts[0].lstrip(':')
+		self.formals = parts[1:]
+
+	def walk(self):
+		...
+
+	def __str__(self):
+		code = 'def ' + self.name + ':\n'
+
+		return code
+
+class ValueExpression:
+	def __init__(self):
+		...
+
+	def walk(self):
+		...
+
+	def __str__(self):
+		...
+
+def isFuncDef(name):
+	return name[0] == ':'
+
+def walk(root, expressions):
+	for child in os.listdir(root):
+		fullpath = root + '/' + child
+		if isFuncDef(child):
+			expressions.append(Definition(child))
 
 def main():
 	root = "./test"
-	walk(root)
+	expressions = []
+	walk(root, expressions)
+	for expr in expressions:
+		print(expr)
 
 if __name__ == "__main__":
 	main()
